053d956fbc7894bbdb71d4f7d03a5a71aa01d105
valivia <valivia@xayania.com>
2023-04-11 14:39:58 +0000

    removed old blades

--- resources/views/_card.blade.php
+++
@@ -1,8 +0,0 @@
-<article class="card">
-            <header>
-                <h2> patient1 </h2>
-                <p>naam:</p>
-                <p>geboortedatum</p>
-                <p>meh</p>
-            </header>
-        </article>
