43b21450d8d6b8314177bde5148012eff6ee8d29
Cas1311 <cashoogerbeets@gmail.com>
2023-04-12 14:26:46 +0200

    patient store

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -2,83 +2,65 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Patient;
 use Illuminate\View\View;
+use Illuminate\Http\Request;
 
 class PatientController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    // Show list all patients of user
     public function index()
     {
-        //
+        // fetch user -> patients with that user id
+        
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    // return view
     public function create(): View
     {
         return view('pages/patient/create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    // post request. validate form and put in db with user id
     public function store(Request $request)
     {
-        //
+        $formFields = $request->validate([
+            'first_name' => ['required', 'min:2'],
+            'last_name' => ['required', 'min:2'],
+            'address' => ['required'],
+            'birthday' => ['required']
+        ]);
+
+        $formFields["user_id"] = auth()->user()->id;
+
+        $patient = Patient::create($formFields);
+
+        return redirect('/patient/' . $patient->id);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    // single patient. return view 'pages/patient/show'
+    public function show(Patient $patient)
+    {
+        // dd($patient);
+        return view("pages/patient/show", [
+            "patient" => $patient
+        ]);
+    }
+
+    // return view
+    public function edit($patient)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    // put request
+    public function update(Request $request, $patient)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
+    // delete patient
+    public function destroy($patient)
     {
         //
     }
