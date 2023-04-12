8b0651530f0bf7a543127d635f668bef26348245
Cas1311 <cashoogerbeets@gmail.com>
2023-04-12 16:07:47 +0200

    cleaned up hardwarecode

--- _client/hardwarecode.ino
+++ _client/hardwarecode.ino
@@ -1,15 +1,8 @@
-// #include <ESP32Servo.h>
-// #include <analogWrite.h>
-// #include <tone.h>
-// #include <ESP32Tone.h>
-// #include <ESP32PWM.h>
 
 #include <Stepper.h>
-#include <Servo.h>
 
 const int stepsPerRevolution = 2048; 
-Servo myservo;
-
+const int checkDelay = 300000;
 
 #define IN1 19
 #define IN2 18
@@ -22,34 +15,20 @@ int buttonState = 0;
 int pos = 0;
 
 void buttonPressed(){
-  // Servo
-  for (pos = 0; pos <= 90; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-  // Stepper
+  // Stepper turn 1 slot
    Serial.println("clockwise");
-  myStepper.step(stepsPerRevolution / 7);
+  myStepper.step(stepsPerRevolution / 15);
+  // Cooldown
   delay(1000);
-  // Servo 2
-  for (pos = 90; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
 }
 
 void setup() {
-  myservo.attach(13);
   myStepper.setSpeed(5);
-  Serial.begin(115200);
   pinMode(buttonPin, INPUT_PULLUP);
 }
 
 void loop() {
-
   buttonState = digitalRead(buttonPin);
-  Serial.println(buttonState);
   if (buttonState == LOW) {
     buttonPressed();
   }
