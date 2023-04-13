81e56aa99459142ccc7c1f84970653c04d506ad1
valivia <valivia@xayania.com>
2023-04-13 13:37:44 +0000

    added creation form

--- resources/views/pages/timeslot/create.blade.php
+++ resources/views/pages/timeslot/create.blade.php
@@ -1,6 +1,42 @@
 @extends('index')
 
 @section('content')
-@endsection
+    @include('partials.nav')
+    <h1>Create Timeslot</h1>
 
-@include('partials.nav')
+    <form class="form" method="POST" action="{{ route('patient.timeslot.store', $patient) }}">
+        @method('POST')
+        @csrf
+
+
+        @include('partials/input', [
+            'name' => 'hour',
+            'label' => 'Hour',
+            'type' => 'number',
+            'value' => (int) date('G'),
+            'required' => true,
+        ])
+
+        @include('partials/input', [
+            'name' => 'minute',
+            'label' => 'Minute',
+            'type' => 'number',
+            'value' => (int) date('i'),
+            'required' => true,
+        ])
+
+        @include('partials/input', [
+            'name' => 'day',
+            'label' => 'Day',
+            'type' => 'number',
+            'value' => (int) date('d'),
+            'required' => true,
+        ])
+
+
+        <button class="button" type="submit" data-variant="primary">
+            Create Patient
+        </button>
+
+    </form>
+@endsection
