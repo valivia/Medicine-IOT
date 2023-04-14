4071c518f4f3f405bb28ffa0610381fdb571516b
ThijsStok <s1094272@student.hsleiden.nl>
2023-04-14 15:28:54 +0200

    esp32 v3

--- app/esp32_connection/esp32_connection.ino
+++ app/esp32_connection/esp32_connection.ino
@@ -1,99 +1,94 @@
-/**
- * BasicHTTPClient.ino
- *
- *  Created on: 24.05.2015
- *
- */
-
 #include <Arduino.h>
-
 #include <WiFi.h>
 #include <WiFiMulti.h>
 #include <ezButton.h>
 #include <HTTPClient.h>
+#include <Stepper.h>
 ezButton button(23);
 #define USE_SERIAL Serial
-
+   /*Variable to store photoresistor value*/
+#define sensor 34
+const int stepsPerRevolution = 2048; 
+#define IN1 19
+#define IN2 18
+#define IN3 5
+#define IN4 17
+int LDR_Val = 0;
 WiFiMulti wifiMulti;
 
-const int LED_PIN = 22;
-int led_state = 0;
+// const int LED_PIN = 22;
+// int led_state = 0;
 
-long start_time = 0;
-long REQUEST_INTERVAL_TIME = 1000;
+// long start_time = 0;
+// long REQUEST_INTERVAL_TIME = 5000;
+
+Stepper myStepper(stepsPerRevolution, IN1, IN3, IN2, IN4);
+const int buttonPin = 4;
+int buttonState = 0;
+int pos = 0;
 
 void setup() {
 
     USE_SERIAL.begin(115200);
     button.setDebounceTime(100);
-    pinMode(LED_PIN, OUTPUT);
-    digitalWrite(LED_PIN, 0);
-
-    USE_SERIAL.println();
-    USE_SERIAL.println();
-    USE_SERIAL.println();
-
+    // pinMode(LED_PIN, OUTPUT);
+    // digitalWrite(LED_PIN, 0);
+    int LDR_Val = analogRead(sensor);
+    myStepper.setSpeed(5);
+    Serial.begin(115200);
+    pinMode(buttonPin, INPUT_PULLUP);
     for(uint8_t t = 4; t > 0; t--) {
         USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
         USE_SERIAL.flush();
         delay(1000);
     }
 
-    wifiMulti.addAP("AndroidAP", "12345678");
-
-    start_time = millis();
+    wifiMulti.addAP("NETGEAR_11N", "3C6b18f1ed");
+    // start_time = millis();
 }
 
-int enough_time_passed(){
-  if(millis() > start_time + REQUEST_INTERVAL_TIME){
-    start_time = millis();
-    return true;
-  } else{
-    return false;
-  }
-}
+// int enough_time_passed(){
+//   if(millis() > start_time + REQUEST_INTERVAL_TIME){
+//     start_time = millis();
+//     return true;
+//   } else{
+//     return false;
+//   }
+// }
 
-void set_remote_button(){
-  // wait for WiFi connection
-  if((wifiMulti.run() == WL_CONNECTED)) {
-
+void lightstatus(){
+  if(LDR_Val >= 10) {
+   if((wifiMulti.run() == WL_CONNECTED)) {
     HTTPClient http;
-
-
       // configure traged server and url
-    http.begin("http://poop3.local/draai");
 
-
-
+    http.begin("http://iot.hootsifer.com/device/123/reset");
       // start connection and send HTTP header
+
     int httpCode = http.GET();
+    Serial.println(httpCode);
 
       // httpCode will be negative on error
     if(httpCode > 0) {
           
-
       // file found at server
       if(httpCode == HTTP_CODE_OK) {
         String payload = http.getString();
-        USE_SERIAL.println(payload);
+        // led_state = payload.toInt();
+        // set_led(led_state);
+        // USE_SERIAL.println(led_state);
       }
     } else {
         // USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
     }
 
     http.end();
-  }
+  }}
 }
 
-void set_led(int led_state){
-  if (led_state == 1){
-    digitalWrite(LED_PIN, HIGH);
-  }
-  else {digitalWrite(LED_PIN, LOW);}}
+void get_timeslot(){
 
-void get_remote_led_state(){
-
-  if(!enough_time_passed()) {
+  if(!get_timeslot()) {
     return;
   }
 
@@ -103,8 +98,7 @@ void get_remote_led_state(){
 
 
       // configure traged server and url
-    http.begin("http://poop3.local/get_led_state");
-
+    http.begin("http://iot.hootsifer.com/device/123/should_open");
 
 
       // start connection and send HTTP header
@@ -117,8 +111,8 @@ void get_remote_led_state(){
       // file found at server
       if(httpCode == HTTP_CODE_OK) {
         String payload = http.getString();
-        led_state = payload.toInt();
-        set_led(led_state);
+        // led_state = payload.toInt();
+        // set_led(led_state);
         // USE_SERIAL.println(led_state);
       }
     } else {
@@ -129,11 +123,21 @@ void get_remote_led_state(){
   }
 }
 
+void buttonPressed(){
+  // Stepper
+   Serial.println("clockwise");
+  myStepper.step(stepsPerRevolution / 15);
+  delay(1000);
+}
+
+
+
 void loop() {
-  button.loop();
-  if (button.isPressed()) {
-    set_remote_button();
-    Serial.println("button");
+  buttonState = digitalRead(buttonPin);
+  Serial.println(buttonState);
+  if (buttonState == LOW) {
+    buttonPressed();
   }
-  get_remote_led_state();
+  int LDR_Val = analogRead(sensor);
+  lightstatus(LDR_Val);
 }
