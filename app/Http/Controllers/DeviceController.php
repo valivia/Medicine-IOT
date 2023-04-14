4443297530f0f49efb4fb769786ef710eb97a825
valivia <valivia@xayania.com>
2023-04-14 19:15:42 +0000

    changed communication protocol
    Co-authored-by: ThijsStok <ThijsStok@users.noreply.github.com>

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -4,38 +4,81 @@ namespace App\Http\Controllers;
 
 use App\Models\Patient;
 use App\Models\Timeslot;
+use DateTime;
+use Facade\FlareClient\Http\Response;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class DeviceController extends Controller
 {
-    public function should_open($id)
-    {
-        // Get the patient and timeslots
-        $patient = Patient::where("device_id", $id)->first();
-        if ($patient) return response(404);
 
-        $timeslot = $patient->timeslots()->where("day", date("w"))->all();
+    public function index($id)
+    {
+        $patient = Patient::where("device_id", $id)->first();
+        if (!$patient) return response(404);
+
+        $shouldOpen = $this->should_open($patient);
+        $patientInfo = [
+            "should_seek" => $patient->should_seek,
+            "should_refill" => $patient->should_refill,
+            "rotate" => $this->rotate($patient),
+        ];
+
+        return Response()->json(array_merge($patientInfo, $shouldOpen));
+    }
+
+    private function should_open(Patient $patient)
+    {
+        $timeslots = $patient->timeslots->sort(
+            function ($a, $b) {
+                $aTime = new DateTime("{$a->day}-01-2023 {$a->hour}:{$a->minute}:00");
+                $bTime = new DateTime("{$b->day}-01-2023 {$b->hour}:{$b->minute}:00");
+                return $aTime <=> $bTime;
+            }
+        )->all();
 
         // Find the timeslot that matches the current time
-        $timeslot;
-        foreach ($timeslot as $t) {
-            if ($t->hour == date("H") && $t->minute == date("i")) {
+        $current_index = -1;
+        $max_index = count($timeslots) - 1;
+        $should_open = false;
+
+
+        $now = new DateTime();
+
+
+        $timeslot = null;
+        foreach ($timeslots as $t) {
+            $tTime = new DateTime("{$t->day}-01-2023 {$t->hour}:{$t->minute}:00");
+            $cTime = new DateTime("{$t->day}-01-2023 {$now->format('H:i')}:00");
+
+
+            if ($tTime < $cTime) {
+                $current_index++;
+                continue;
+            }
+
+            if ($tTime == $cTime) {
                 $timeslot = $t;
                 break;
             }
         }
 
+
+
         // If there is no timeslot, return 404
-        if ($timeslot == null) {
-            return response(204);
+        if ($timeslot) {
+            $should_open = true;
+            $timeslot->received = true;
+            $timeslot->save();
         }
 
         // If there is a timeslot store that the device received the request
-        $timeslot->received = true;
-        $timeslot->save();
 
-        return response(200);
+        return [
+            "should_open" => $should_open,
+            "current_index" => $current_index,
+            "max_index" => $max_index,
+        ];
     }
 
     public function reset($id)
@@ -53,99 +96,4 @@ class DeviceController extends Controller
 
         return response(200);
     }
-
-    // Seek
-    public function should_seek($id)
-    {
-        // Get the patient.
-        $patient = Patient::where("device_id", $id)->first();
-
-        // If the patient should seek, return 200.
-        if ($patient->should_seek) {
-            $patient->should_seek = false;
-            $patient->save();
-            return response(200);
-        }
-
-        // If the patient should not seek, return 204.
-        return response(204);
-    }
-
-    public function set_should_seek($id)
-    {
-        // Get the patient.
-        $patient = Patient::where("device_id", $id)->first();
-
-        // Set the patient to seek.
-        $patient->should_seek = true;
-        $patient->save();
-
-        return redirect()->back();
-    }
-
-    // Refill
-
-    public function should_refill($id)
-    {
-        // Get the patient.
-        $patient = Patient::where("device_id", $id)->first();
-
-        // If the patient should refill, return 200.
-        if ($patient->should_refill) {
-            $patient->should_refill = false;
-            $patient->save();
-            return response(200);
-        }
-
-        // If the patient should not refill, return 204.
-        return response(204);
-    }
-
-    public function set_should_refill($id)
-    {
-        // Get the patient.
-        $patient = Patient::where("device_id", $id)->first();
-
-        // Set the patient to refill.
-        $patient->should_refill = true;
-        $patient->save();
-
-        return redirect()->back();
-    }
-
-    // Rotate
-    public function rotate($id)
-    {
-        // Get the patient.
-        $patient = Patient::where("device_id", $id)->first();
-
-        // If the patient should rotate, return 200.
-        if ($patient->rotate != 0) {
-            $direction = $patient->rotate;
-            $patient->rotate = 0;
-            $patient->save();
-            return response($direction, 200);
-        }
-
-        // If the patient should not rotate, return 204.
-        return response(204);
-    }
-
-    public function set_rotate($id, $direction)
-    {
-        // Get the patient.
-        $patient = Patient::where("device_id", $id)->first();
-
-        if ($direction != 1 && $direction != -1) {
-            return response(400);
-        }
-
-        // Set the patient to rotate.
-        $patient->rotate = $direction;
-        $patient->save();
-
-        return redirect()->back();
-    }
 }
-
-
--- routes/web.php
+++ routes/web.php
@@ -36,20 +36,4 @@ Route::resource("/patient", PatientController::class)->middleware("auth");
 Route::resource("patient.timeslot", TimeslotController::class)->middleware("auth");
 Route::resource("patient.medication", MedicationController::class)->middleware("auth");
 
-// ping every 5 seconds to see if should be ready to open
-Route::get("/device/{id}/should_open", [DeviceController::class, "should_open"]);
-
-// pings when the caretaker refills the box.
-Route::get("/device/{id}/reset", [DeviceController::class, "reset"]);
-
-// rotate
-Route::get("/device/{id}/rotate", [DeviceController::class, "rotate"])->name("device.rotate");
-Route::post("/device/{id}/rotate/{direction}", [DeviceController::class, "set_rotate"])->name("device.rotate");
-
-// seek
-Route::get("/device/{id}/should_seek", [DeviceController::class, "should_seek"])->name("device.seek");
-Route::post("/device/{id}/should_seek", [DeviceController::class, "set_should_seek"])->name("device.seek");
-
-// refill
-Route::get("/device/{id}/should_refill", [DeviceController::class, "should_refill"])->name("device.refill");
-Route::post("/device/{id}/should_refill", [DeviceController::class, "set_should_refill"])->name("device.refill");
+Route::get("/device/{id}", [DeviceController::class, "index"]);
