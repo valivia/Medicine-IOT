aebb959d3ea3f9fd257dadcd352da403e487ff34
Cas1311 <cashoogerbeets@gmail.com>
2023-04-14 10:43:17 +0200

    waarom heb je mn logout weggehaald😠

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -2,6 +2,6 @@
     <div class="navMain">
         <a class="back" href="{{ url()->previous() }}">@include('/partials/icons/back') Back </a>
         <a href="{{ route('patient.index') }}">@include('/partials/icons/home') Home</a>
-        {{-- <li><a href="{{ route('logout') }}">@include('/partials/icons/signout')</a></li> --}}
+        {{-- <a href="{{ route('logout') }}">@include('/partials/icons/signout')Logout</a> --}}
     </div>
 </nav>
